e1eb748d7544a605435b3213d0d08b9b66e73a82
Angel <angelo_stoyanov@abv.bg>
2023-05-03 17:16:46 +0300

    load esri map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import esri = __esri; // Esri types
+import { loadModules } from 'esri-loader';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
-
+export class MapComponent implements AfterViewInit {
+  @ViewChild('map', { static: false })
+  private mapElementRef?: ElementRef;
+  mapView?: esri.MapView;
+  map?: esri.Map;
+  defaultCenterLat: number = 42.697866831005435;//LOCATION SOFIA
+  defaultCenterLon: number = 23.321590139866355;//LOCATION SOFIA
+  defaultZoom: number = 10;
+  defaultBaseMap: string = 'streets-vector';
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initDefaultMap();
   }
 
+
+
+  private async initDefaultMap(): Promise<void> {
+    const [Map, MapView] = await loadModules(['esri/WebMap', 'esri/views/MapView']);
+    this.map = new Map({
+      basemap: this.defaultBaseMap,
+    });
+
+    this.mapView = new MapView({
+      map: this.map,
+      center: [this.defaultCenterLon, this.defaultCenterLat],
+      zoom: this.defaultZoom,
+      container: this.mapElementRef?.nativeElement,
+      ui: {
+        components: ['attribution'],
+      },
+    });
+  }
 }
